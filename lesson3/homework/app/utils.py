726e4c24150a8d9ff3fc26282391d46657752f3e
k.barylnikov <ipt_git_bot@ipt.ru>
2022-02-18 13:29:33 +0300

    hw3

---
+++ lesson3/homework/app/utils.py
@@ -0,0 +1,22 @@
+import json
+from socket import socket
+
+
+def send_message(s: socket, message: dict):
+    message_str = json.dumps(message)
+    return s.send(message_str.encode('utf-8'))
+
+
+def receive_message(s: socket):
+    message = s.recv(1000)
+    return json.loads(message.decode('utf-8'))
+
+
+def check_port(port: str):
+    if port.isdigit() and 1024 <= int(port) <= 65535:
+        return int(port)
+    raise ValueError('указан некорректный порт')
+
+
+def print_message(message: dict):
+    print(f'got message {json.dumps(message, indent=4)}')
